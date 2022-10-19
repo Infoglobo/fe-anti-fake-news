3b68dbbac86cf6499f607d62aebe5340450f6a3e
Jorge Rafael <eu.jorgerafael@gmail.com>
2022-10-19 13:49:30 -0300

    Adicionado uma camada de dificuldade no código

--- anti-fake-news.js
+++ anti-fake-news.js
@@ -1,19 +1,30 @@
 'use strict'
 
-const elementListeners = [
-    '.class-selector',
-    '#id-selector'
-]
+const elementListeners = []
 
 const img = ''
 
-elementListeners.forEach(el => {
-    console.log(el);
-    document.querySelector(el).addEventListener('DOMCharacterDataModified', (e) => {
-        changeBg()
-        //console.log(e)
+
+if (elementListeners.length) {
+    elementListeners.forEach(el => {
+        document.querySelector(el).addEventListener('DOMCharacterDataModified', (e) => {
+            changeBg()
+        })
     })
-})
+}
+else {
+    document.addEventListener('DOMCharacterDataModified', (e) => {
+        document.querySelectorAll('*').forEach((el) => {
+            if (img) {
+                el.style.backgroundImage = `url("${img}")`, el.style.repeat = "repeat"
+            }
+            else {
+                el.style.background = 'red';
+            }
+        })
+    })
+}
+
 
 function changeBg() {
     document.body.style.backgroundImage = `url("${img}")`, document.body.style.repeat = "repeat"
